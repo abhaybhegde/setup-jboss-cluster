d3649b02e0867d2a6724138d7a3edfb436b312fa
abhay <abhaybhegde@gmail.com>
2017-03-25 20:42:20 +0530

    Adding TravisCI dependent files

---
+++ run_tests.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+#Travis-CI script to make it easier to run all the unit-test 
+
+PROJECT_ROOT=`pwd` 
+echo "Currently in $PROJECT_ROOT"
+cd $PROJECT_ROOT/tests
+export PYTHONPATH=$PROJECT_ROOT/health_check:$PROJECT_ROOT/configure_slaves
+python -m unittest test_check_network_connectivity
+python -m unittest test_slave
